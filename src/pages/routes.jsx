ed50918906d5dc0825675871d954bd1e7b350815
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-10-30 19:04:32 +0300

    fix errors

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,65 +1,69 @@
-import React, { createContext, useMemo } from 'react';
-import { useRoutes } from 'react-router-dom';
-import { useTheme } from '../hooks/useTheme';
+import React, { createContext, useMemo } from "react";
+import { useRoutes } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
-import { Main } from './main';
-import { DashBoard } from './dashboard';
-import { Achievements } from './achievements';
-import { Account } from './account';
-import { Login } from './login';
-import { CalendarPage } from './calendar';
-import { About } from './about';
+import { Main } from "./main";
+import { DashBoard } from "./dashboard";
+import { Achievements } from "./achievements";
+import { Account } from "./account";
+import { Login } from "./login";
+import { CalendarPage } from "./calendar";
+import { About } from "./about";
 
-import Calendar from '../components/CalendarDark/Calendar';
-import { About } from './about';
-import { ThemeContext } from './context';ture/calendarPage
+import Calendar from "../components/CalendarDark/Calendar";
+
+import { ThemeContext } from "./context";
 
 const routes = [
-    {
-        path: '/',
-        element: <Main />,
-    },
-    {
-        path: '/dashboard',
-        element: <DashBoard />,
-    },
-    {
-        path: '/achievements',
-        element: <Achievements />,
-    }, {
-        path: '/account',
-        element: <Account />,
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
-    {
-        path: '/main',
-        element: <Calendar />,
-    },
-    {
-        path: '/about',
-        element: <About />,
-    },
-    {
-        path: '/calendar',
-        element: <CalendarPage />,
-    }
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard />,
+  },
+  {
+    path: "/achievements",
+    element: <Achievements />,
+  },
+  {
+    path: "/account",
+    element: <Account />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/main",
+    element: <Calendar />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/calendar",
+    element: <CalendarPage />,
+  },
 ];
 
 export const AppRouter = () => {
-    const { theme, setTheme } = useTheme();
-    const component = useRoutes(routes);
+  const { theme, setTheme } = useTheme();
+  const component = useRoutes(routes);
 
-    const themeContext = useMemo(() => ({
-        theme,
-        setTheme,
-    }), [setTheme, theme]);
+  const themeContext = useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [setTheme, theme]
+  );
 
-    return (
-        <ThemeContext.Provider value={themeContext}>
-            {component}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={themeContext}>
+      {component}
+    </ThemeContext.Provider>
+  );
 };
