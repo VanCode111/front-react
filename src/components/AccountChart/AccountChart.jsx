7ea73a92810331f3a42d216702ee68e437076878
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-10-30 19:00:11 +0300

    add mock data

--- src/components/AccountChart/AccountChart.jsx
+++ src/components/AccountChart/AccountChart.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
+import React from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
     },
+  },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = [
+  "янв",
+  "фев",
+  "март",
+  "апр",
+  "май",
+  "июнь",
+  "июль",
+  "авг",
+  "сент",
+  "окт",
+  "ноябрь",
+  "дек",
+];
 
-export const data = {
+export const AccountChart = ({ activity }) => {
+  const data = {
     labels,
     datasets: [
-        {
-            label: 'Статистика посещаемости',
-            data: [1000, 10, -123, 13, 1000, 22],
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
+      {
+        label: "Статистика посещаемости",
+        data: activity,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
     ],
-};
+  };
 
-export const AccountChart = () => <Line options={options} data={data} />;
+  return <Line options={options} data={data} />;
+};
--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,43 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import { AdminPage } from '../../containers/AdminPage/AdminPage';
-import { Card } from '../../containers';
-import { CircleChart } from '../../components/atomic/CircleChart/CircleChart';
-import { AccountChart } from '../../components/AccountChart/AccountChart';
-import { AccountForm } from '../../components/AccountForm/AccountForm';
+import { AdminPage } from "../../containers/AdminPage/AdminPage";
+import { Card } from "../../containers";
+import { CircleChart } from "../../components/atomic/CircleChart/CircleChart";
+import { AccountChart } from "../../components/AccountChart/AccountChart";
+import { AccountForm } from "../../components/AccountForm/AccountForm";
+import { statistics } from "../../mock/account";
+import classes from "./account.module.css";
 
-import classes from './account.module.css';
+export const Account = () => {
+  const tasks = statistics?.tasks || 0;
+  const goals = statistics?.goals || 0;
 
-export const Account = () => (
-    <AdminPage title={'Личный кабинет'}>
-        <AccountForm />
-        <Card className={classes.graphicContainer}>
-            <div className={classes.graphic}>
-                <h2 className={classes.title}>Ваша активность</h2>
-                <AccountChart />
+  return (
+    <AdminPage title={"Личный кабинет"}>
+      <AccountForm />
+      <Card className={classes.graphicContainer}>
+        <div className={classes.graphic}>
+          <h2 className={classes.title}>Ваша активность</h2>
+          <AccountChart activity={statistics.activity} />
+        </div>
+        <div className={classes.circleWrapper}>
+          <h2 className={classes.title}>
+            Соотношение глобальных целей и задач:
+          </h2>
+          <div className={classes.circleContainer}>
+            <div className={classes.circleData}>
+              <div className={classes.circleDataItem}>
+                <div
+                  className={classes.circle}
+                  style={{ backgroundColor: "#F8D564" }}
+                />
+                Задачи
+              </div>
+              <div
+                className={classes.circleDataItem}
+                style={{ color: "rgba(66, 66, 66, 0.5)" }}
+              >
+                {goals}%
+              </div>
+              <div className={classes.circleDataItem}>
+                <div
+                  className={classes.circle}
+                  style={{ backgroundColor: "#AB5DE9" }}
+                />
+                Цели
+              </div>
+              <div
+                className={classes.circleDataItem}
+                style={{ color: "rgba(66, 66, 66, 0.5)" }}
+              >
+                {tasks}%
+              </div>
             </div>
-            <div className={classes.circleWrapper}>
-                <h2 className={classes.title}>Соотношение глобальных целей и задач:</h2>
-                <div className={classes.circleContainer}>
-                    <div className={classes.circleData}>
-                        <div className={classes.circleDataItem}>
-                            <div className={classes.circle} style={{ backgroundColor: '#F8D564' }} />
-                            Задачи
-                        </div>
-                        <div className={classes.circleDataItem} style={{ color: 'rgba(66, 66, 66, 0.5)' }}>
-                            64%
-                        </div>
-                        <div className={classes.circleDataItem}>
-                            <div className={classes.circle} style={{ backgroundColor: '#AB5DE9' }} />
-                            Цели
-                        </div>
-                        <div className={classes.circleDataItem} style={{ color: 'rgba(66, 66, 66, 0.5)' }}>
-                            36%
-                        </div>
-                    </div>
-                    <CircleChart data={[36, 64]} colors={['#AB5DE9', '#F8D564']} labels={['Телефон', 'Компьютер']} />
-                </div>
-            </div>
-        </Card>
+            <CircleChart
+              data={[tasks, goals]}
+              colors={["#AB5DE9", "#F8D564"]}
+              labels={["Телефон", "Компьютер"]}
+            />
+          </div>
+        </div>
+      </Card>
     </AdminPage>
-);
+  );
+};
