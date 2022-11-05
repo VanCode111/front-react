9fa81605686b4f0a213ed0ad56dfe3be00ffffcf
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-11-05 17:41:53 +0300

    fix login page

--- src/components/atomic/Input/Input.jsx
+++ src/components/atomic/Input/Input.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import cn from 'classnames';
-import { PropTypes } from 'prop-types';
+import React from "react";
+import cn from "classnames";
+import { PropTypes } from "prop-types";
 
-import classes from './Input.module.css';
+import classes from "./Input.module.css";
 
 export const Input = React.forwardRef(
-    ({
-        type = 'text', className = '', variant, ...props
-    }, ref) => (
-        <input
-            className={cn(classes.input, className, classes[`variant_${variant}`])}
-            type={type}
-            {...props}
-            ref={ref}
-        />
-    ),
+  ({ type = "text", className = "", variant, color, ...props }, ref) => (
+    <input
+      className={cn(
+        classes.input,
+        className,
+        classes[`variant_${variant}`],
+        classes[`color_${color}`]
+      )}
+      type={type}
+      {...props}
+      ref={ref}
+    />
+  )
 );
 
 Input.propTypes = {
-    placeholder: PropTypes.string,
-    type: PropTypes.string,
-    className: PropTypes.string,
-    variant: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  className: PropTypes.string,
+  variant: PropTypes.string,
 };
