e74179ac8925caac48a37a403d0b320cc6822e3c
Илья <92045360+000Ilya000@users.noreply.github.com>
2022-11-12 19:10:18 +0400

    regist-newtask

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useMemo } from "react";
 import { useRoutes } from "react-router-dom";
 import { useTheme } from "../hooks/useTheme";
 
+import { Registration } from "./Registration"
+import { NewTask } from "./Newtask";
 import { Main } from "./main";
 import { DashBoard } from "./dashboard";
 import { Achievements } from "./achievements";
@@ -56,6 +58,14 @@ const routes = [
   {
     path: "/users",
     element: <Users />,
+  },
+  {
+    path: "/newtask",
+    element: <NewTask />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
   },
 ];
 
