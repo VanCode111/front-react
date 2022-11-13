51be719b6467d64aa6177cc0f87590f024d310c6
evgenylutsan <evgeny_lutsan@mail.ru>
2022-11-13 20:01:18 +0300

    footer

--- src/components/templates/Footer/Footer.jsx
+++ src/components/templates/Footer/Footer.jsx
@@ -8,12 +8,12 @@ export const Footer = () => (
     <footer className={styles.footer}>
         <div className={styles.container}>
             <div className={styles.footer__links}>
-                <img width={112} height={112} src="images/logo.svg" alt="Logo" />
+                <img className={styles.img} width={112} height={112} src="images/logo.svg" alt="Logo" />
                 <ul>
                     <li>
                         <Link to="#">Скачать приложение</Link>
                     </li>
-                    <li>
+                    <li className={styles.adaptive}>
                         <Link to="#">О нас</Link>
                     </li>
                     <li>
